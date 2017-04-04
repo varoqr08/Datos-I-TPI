e463573bb110f0ca34bf9b51180855d014232a9d
Gabs42 <gachix00@hotmail.com>
2017-04-03 22:26:35 -0700

    PowerUps y Movimiento Enemigos

--- AirWars/src/Mapa.java
+++ AirWars/src/Mapa.java
@@ -10,45 +10,51 @@ import java.util.Random;
  * Created by Gabriel on 3/20/2017.
  */
 public class Mapa extends JPanel implements ActionListener,KeyListener{
-    Timer timer = new Timer(5,this);//un timer para comenzar el juego
-    Timer timeEnemigos = new Timer(1000, new ActionListener() {
+    private Timer timer = new Timer(5,this);//un timer para comenzar el juego
+    private Timer timeEnemigos = new Timer(1000, new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             condicionEnemigos = true;
         }
     });//timer para que aparezca enemgios
-    Timer timeBalas = new Timer(2000, new ActionListener() {
+    private Timer timeBalas = new Timer(2000, new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             balasEnemigos = true;
         }
     });//timer para que enemigos disparen
-    Timer timeMov = new Timer(1000, new ActionListener() {
+    private Timer timeMov = new Timer(1000, new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             movEnemigos = true;
         }
     });//timer para que enemigos se muevan
-    Timer timeAlimentarCola = new Timer(1000, new ActionListener() {
+    private Timer timeAlimentarCola = new Timer(1000, new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             alimentarCola = true;
         }
     });//timer para que enemigos se muevan
-    Timer timeChoqueEnemigos = new Timer(1000, new ActionListener() {
+    private Timer timeChoqueEnemigos = new Timer(1000, new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             choqueEnemigos = true;
         }
     });
-    Player Jugador = new Player();//creacion de jugador
-    int cambX = 0, cambY = 0, velBalas = -3,cantBalas=0, cantBalasEnemigo = 0, cantEnemigosTotal =0, contadorCantEnemigos = 0,PosEnemX=0,PosEnemY=0;//valores de cambio de jugador y cantidad de balas de enemigos o jugadores
-    Boolean condicionBalas = false, MovBala = false, condicionEnemigos = false,balasEnemigos = false, movEnemigos = false, alimentarCola = false,choqueEnemigos =false,pintarPowerUps =false;//balas,movimient balas,creacion enemigos,creacion balas enemigos,moviemiento de enemigos
-    LinkList<Bala> listaBalas = new LinkList();//lista de balas
-    Cola colaEnemigos = new Cola();//cola de enemigos activos
-    Cola colaEspera  = new Cola();//cola de enemigos que no han entrado al juego
-    Pila pilaPowerUps = new Pila();
-    Random generador = new Random();
+    private Timer timeEscudo = new Timer(5000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            condicionEscudo = false;
+        }
+    });
+    private Player Jugador = new Player();//creacion de jugador
+    private int cambX = 0, cambY = 0, velBalas = -3,cantBalas=0, cantBalasEnemigo = 0, cantEnemigosTotal =0, contadorCantEnemigos = 0,PosEnemX=0,PosEnemY=0,balasEspeciales = 0,sizePowerUps = 0, contadorCicloPowerUps=1;//valores de cambio de jugador y cantidad de balas de enemigos o jugadores
+    private Boolean condicionBalas = false, MovBala = false, condicionEnemigos = false,balasEnemigos = false, movEnemigos = false, alimentarCola = false,choqueEnemigos =false,pintarPowerUps =false,condicionEscudo = false;//balas,movimient balas,creacion enemigos,creacion balas enemigos,moviemiento de enemigos
+    private LinkList<Bala> listaBalas = new LinkList();//lista de balas
+    private Cola colaEnemigos = new Cola();//cola de enemigos activos
+    private Cola colaEspera  = new Cola();//cola de enemigos que no han entrado al juego
+    private Pila pilaPowerUps = new Pila();
+    private Random generador = new Random();
     public Mapa(int cantEnenemigos){
         cantEnemigosTotal = cantEnenemigos;
         int x=0;
@@ -72,11 +78,38 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
     //INICION LOGICA PRINCIPAL DEL JUEGO
     public void paintComponent(Graphics g){
         super.paintComponent(g);//Paint sale de JPanel esto se asegura que salga igual en cualquier computadora
-        g.setColor(Color.RED);//Color del jugador
+        if(condicionEscudo==true){//revisa si se tiene escudo
+            g.setColor(Color.cyan);
+            timeEscudo.start();
+        }
+        else {
+            g.setColor(Color.RED);//Color del jugador
+        }
         g.fillRect(Jugador.getPosX(),Jugador.getPosY(),50,30);//primer dato cantidad de pixeles que se mueve a la Derecha segundo dato cantidad de pixeles que se mueve  abajo los ultmos dos numeros son las dimensiones del cuadrado
-        g.setColor(Color.GRAY);//Barra de vida
+        g.setColor(Color.PINK);//Barra de vida
         g.fillRect(0,461,Jugador.getVidas(),30);//posicion de la barra de vida
-
+        while(contadorCicloPowerUps<=3){
+            if(contadorCicloPowerUps<=sizePowerUps){
+                PowerUps PU = pilaPowerUps.getTope().getDato();
+                pilaPowerUps.pop();
+                if(PU.getTipo()==1){
+                    g.setColor(Color.CYAN);
+                }
+                if(PU.getTipo()==2){
+                    g.setColor(Color.GREEN);
+                }
+                if(PU.getTipo()==3){
+                    g.setColor(Color.MAGENTA);
+                }
+                pilaPowerUps.push(PU);
+            }
+            else {
+                g.setColor(Color.GRAY);
+            }
+            g.fillRect(100+(contadorCicloPowerUps*20),461,15,30);
+            contadorCicloPowerUps+=1;
+        }
+        contadorCicloPowerUps=1;
         //LOGICA POWERUPS
         if(pintarPowerUps==true){
             PowerUps powerUp = new PowerUps(PosEnemX,PosEnemY);
@@ -84,8 +117,12 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
             g.fillOval(powerUp.getPosX(),powerUp.getPosY(),10,10);
             if((Jugador.getPosX()<powerUp.getPosX()&&(Jugador.getPosX()+50)>powerUp.getPosX() )){
                if((Jugador.getPosY()-15)<(powerUp.getPosY()-5)&&(Jugador.getPosY()+15)>(powerUp.getPosY()-10)){
-                    PosEnemY = 0;
-                    PosEnemX = 0;
+                   if(sizePowerUps !=4){
+                       pilaPowerUps.push(powerUp);
+                       PosEnemY = -10;
+                       PosEnemX = -10;
+                       sizePowerUps+=1;
+                   }
                 }
             }
         }
@@ -128,37 +165,72 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
                 contador = 1;
                 while(contador<=sizeColaEnemigos){
                     enemigoActual = colaEnemigos.getInicio();
-                    enemigoActual.getDato().setPosY( enemigoActual.getDato().getPosY()+1);
+                    //LOGICA JETS
+                    if(enemigoActual.getDato().getTipo()==1 || enemigoActual.getDato().getTipo()==2){
+                        enemigoActual.getDato().setPosY( enemigoActual.getDato().getPosY()+1);
+                        if(enemigoActual.getDato().getContadorMovimiento()<0){//Si contador menor a 0 se mueve izq
+                            enemigoActual.getDato().setPosX(enemigoActual.getDato().getPosX()-enemigoActual.getDato().getTipo());
+                            enemigoActual.getDato().setContadorMovimiento(enemigoActual.getDato().getContadorMovimiento()+1);
+                        }
+                        else if(enemigoActual.getDato().getContadorMovimiento()>=0){
+                            enemigoActual.getDato().setPosX(enemigoActual.getDato().getPosX()+enemigoActual.getDato().getTipo());
+                            enemigoActual.getDato().setContadorMovimiento(enemigoActual.getDato().getContadorMovimiento()+1);
+                            if(enemigoActual.getDato().getContadorMovimiento()>30){
+                                enemigoActual.getDato().setContadorMovimiento(-30);
+                            }
+                        }
+                    }
+                    //FIN LOGICA JETS
+
+                    //INICIO LOGICA KAMIKAZE
+                    if(enemigoActual.getDato().getTipo() ==3){
+                        enemigoActual.getDato().setPosY( enemigoActual.getDato().getPosY()+1);
+                        if(enemigoActual.getDato().getPosX()<Jugador.getPosX()){//ve si esta a la izquierda de jugador
+                            enemigoActual.getDato().setPosX(enemigoActual.getDato().getPosX()+1);
+                        }
+                        if(enemigoActual.getDato().getPosX()>Jugador.getPosX()){//ve si esta a la derecha de jugador
+                            enemigoActual.getDato().setPosX(enemigoActual.getDato().getPosX()-1);
+                        }
+                    }
+                    //FIN LOGICA KAMIKAZE
+                    if(enemigoActual.getDato().getPosY()>410){
+                        enemigoActual.getDato().setPosY(0);
+                    }
                     colaEnemigos.encolar(enemigoActual);
                     contador+=1;
+
                 }
             }
             //FIN LOGICA MOVIMIENTO ENEMIGOS
 
             timeBalas.start();//comienza timer para que enemgios dispare
+            //LOGICA BALAS ENEMIGOS
             if(balasEnemigos == true){
                 if(cantBalasEnemigo<1){//cantidad de balas de enemigos que pueden estar en la pantalla
                     contador = 1;
                     while(contador<=sizeColaEnemigos){
-                        Bala bala = new Bala();//crea una bala
-                        listaBalas.insertFirstLink(bala);//se inserta a lista la bala
-                        listaBalas.getEspecifico(0).setAfiliacion(0);
                         enemigoActual = colaEnemigos.getInicio();
-                        int x = enemigoActual.getDato().getPosX();
-                        int y =enemigoActual.getDato().getPosY();
-                        colaEnemigos.encolar(enemigoActual);
-                        listaBalas.getEspecifico(0).setPosX(x+20);
-                        listaBalas.getEspecifico(0).setPosY(y+30);
-                        g.fillOval( listaBalas.getEspecifico(0).getPosX(),listaBalas.getEspecifico(0).getPosY(),10,10);
-                        MovBala = true;
-                        condicionBalas =false;
-                        balasEnemigos = false;
-                        cantBalasEnemigo+=1;
+                        if(enemigoActual.getDato().getTipo()!=3){
+                            Bala bala = new Bala();//crea una bala
+                            listaBalas.insertFirstLink(bala);//se inserta a lista la bala
+                            listaBalas.getEspecifico(0).setDañoBala(enemigoActual.getDato().getTipo());//le asigna la bala el tipo de nave que la disparo para calcular daño
+                            listaBalas.getEspecifico(0).setAfiliacion(0);
+                            int x = enemigoActual.getDato().getPosX();
+                            int y =enemigoActual.getDato().getPosY();
+                            listaBalas.getEspecifico(0).setPosX(x+20);
+                            listaBalas.getEspecifico(0).setPosY(y+30);
+                            g.fillOval( listaBalas.getEspecifico(0).getPosX(),listaBalas.getEspecifico(0).getPosY(),10,10);
+                            MovBala = true;
+                            condicionBalas =false;
+                            balasEnemigos = false;
+                            cantBalasEnemigo+=1;
+                        }
                         contador+=1;
+                        colaEnemigos.encolar(enemigoActual);
                     }
                 }
             }
-
+            //FIN LOGICA BALAS ENEMIGOS
         }
         //FIN LOGICA ENEMIGOS
         //LOGICA GENERACION BALAS JUGADOR
@@ -175,10 +247,18 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
 
         //LOGICA BALAS
         if(MovBala == true){
-            g.setColor(Color.RED);
             int x = 0;//x con el que se recorre lista
             while(x!=listaBalas.getSize()){// este while existe para que se puedan crear mas de 1 bala al mismo tiempo ya qu recorre una lista de balas
                 if (listaBalas.getEspecifico(x).getAfiliacion()==1){//si la bala es aliada
+                    if(listaBalas.getEspecifico(x).getDañoBala()==1){//desgina color segun power up
+                        g.setColor(Color.RED);
+                    }
+                    if(listaBalas.getEspecifico(x).getDañoBala()==2){
+                        g.setColor(Color.GREEN);
+                    }
+                    if(listaBalas.getEspecifico(x).getDañoBala()==3){
+                        g.setColor(Color.MAGENTA);
+                    }
                     g.fillOval(listaBalas.getEspecifico(x).getPosX(),listaBalas.getEspecifico(x).getPosY(),10,10);
                     listaBalas.getEspecifico(x).setPosY(listaBalas.getEspecifico(x).getPosY()+velBalas);
                     //LOGICA COLISION BALAS CON ENEMIGOS
@@ -187,13 +267,16 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
                     while(contador<=sizeColaEnemigos){
                         Nodo enemigoActual = colaEnemigos.getInicio();
                         if(listaBalas.getEspecifico(x).getPosY()<(enemigoActual.getDato().getPosY()+30)&&listaBalas.getEspecifico(x).getPosY()>(enemigoActual.getDato().getPosY())&&listaBalas.getEspecifico(x).getPosX()<(enemigoActual.getDato().getPosX()+50)&&(listaBalas.getEspecifico(x).getPosX()>enemigoActual.getDato().getPosX())){
-                            listaBalas.getEspecifico(x).setPosX(-10);//mueve la bala out of bounds en un lugar no visible que no afecta
-                            listaBalas.getEspecifico(x).setPosY(-10);//mueve la bala out of bounds
+                            if(listaBalas.getEspecifico(x).getDañoBala()!=3){//revisa si la bala es lase si es laser atraviesa enemigos
+                                listaBalas.getEspecifico(x).setPosX(-10);//mueve la bala out of bounds en un lugar no visible que no afecta
+                                listaBalas.getEspecifico(x).setPosY(-10);//mueve la bala out of bounds
+                            }
                             g.fillOval(listaBalas.getEspecifico(x).getPosX(),listaBalas.getEspecifico(x).getPosY(),0,0);
                             cantBalas -=1;
                             //LOGICA CALCULO HP ENEMGIOS
-                            if(enemigoActual.getDato().getHP()>1){
-                                enemigoActual.getDato().setHP(enemigoActual.getDato().getHP()-1);
+                            enemigoActual.getDato().setHP(enemigoActual.getDato().getHP()-listaBalas.getEspecifico(x).getDañoBala());
+                            if(enemigoActual.getDato().getHP()>0){
+
                             }else{
                                 int valor = 1+generador.nextInt(100);
                                 if(valor<=100&&valor>0){
@@ -209,6 +292,7 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
                         contador+=1;
                     }
                     //FIN LOGICA COLISION BALAS CON ENEMIGOS
+
                     //LOGICA BALA ALIADA FUERA DE BORDES
                     if(listaBalas.getEspecifico(x).getPosY()> 0&& listaBalas.getEspecifico(x).getPosY()< 10){//revisa si las balas tocan el borde de arriba de la pantalla
                         listaBalas.getEspecifico(x).setPosX(-10);//mueve la bala out of bounds
@@ -218,12 +302,16 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
                     }
                     //FIN LOGICA BALA FUERA DE BORDES
                 }
+                //LOGICA BALA ENEMIGOS
                 if(listaBalas.getEspecifico(x).getAfiliacion()==0){//si bala es enemiga
+                    g.setColor(Color.RED);
                     g.fillOval(listaBalas.getEspecifico(x).getPosX(),listaBalas.getEspecifico(x).getPosY(),10,10);
                     listaBalas.getEspecifico(x).setPosY(listaBalas.getEspecifico(x).getPosY()-velBalas);
                     //LOGICA BALA CHOCA CONTRA JUGADOR
                     if(listaBalas.getEspecifico(x).getPosY()<(Jugador.getPosY()+30)&&listaBalas.getEspecifico(x).getPosY()>(Jugador.getPosY())&&listaBalas.getEspecifico(x).getPosX()<(Jugador.getPosX()+50)&&(listaBalas.getEspecifico(x).getPosX()>Jugador.getPosX())){
-                        Jugador.setVidas(Jugador.getVidas()-20);
+                        if(condicionEscudo!=true){//revisa si se tiene escudo
+                            Jugador.setVidas(Jugador.getVidas()-(40/listaBalas.getEspecifico(x).getDañoBala()));//se divide entre el tipo ya que tipo 1(bombardero) hace mas daño que tipo 2(jet)
+                        }
                         listaBalas.getEspecifico(x).setPosX(-10);//mueve la bala out of bounds
                         listaBalas.getEspecifico(x).setPosY(-10);//mueve la bala out of bounds
                         g.fillOval(listaBalas.getEspecifico(x).getPosX(),listaBalas.getEspecifico(x).getPosY(),0,0);
@@ -232,6 +320,7 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
                     cantBalasEnemigo-=1;
                     timeBalas.start();
                 }
+                //FINAL LOGICA BALA ENEMIGOS
                 x+=1;
                 }
         }
@@ -241,7 +330,9 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
             Nodo enemigoActual = colaEnemigos.getInicio();
             if((Jugador.getPosX()>enemigoActual.getDato().getPosX()&&Jugador.getPosX()<(enemigoActual.getDato().getPosX()+50)) || ((Jugador.getPosX()+50)>enemigoActual.getDato().getPosX()&&(Jugador.getPosX()+50)<(enemigoActual.getDato().getPosX()+50))){
                 if((Jugador.getPosY()-15)>(enemigoActual.getDato().getPosY()-15)&&(Jugador.getPosY()-15)<(enemigoActual.getDato().getPosY()+15)||(Jugador.getPosY()+15)>(enemigoActual.getDato().getPosY()-15)&&(Jugador.getPosY()+15)<(enemigoActual.getDato().getPosY()+15)){
-                    Jugador.setVidas(Jugador.getVidas()-20);
+                    if(condicionEscudo!=true){
+                        Jugador.setVidas(Jugador.getVidas()-20);
+                    }
                     enemigoActual = null;
                 }
             }
@@ -299,7 +390,14 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
                 Bala bala = new Bala();// se crea una bala
                 listaBalas.insertFirstLink(bala);// se adhiere la bala a la lista de balas
                 listaBalas.getEspecifico(0).setAfiliacion(1);
+                listaBalas.getEspecifico(0).setDañoBala(Jugador.getDañoJugador());
                 condicionBalas = true;
+                if(balasEspeciales>1){
+                    balasEspeciales-=1;
+                }
+                else{
+                    Jugador.setDañoJugador(1);
+                }
 
             }
             else{
@@ -307,7 +405,22 @@ public class Mapa extends JPanel implements ActionListener,KeyListener{
             }
         }
         if(tecla == KeyEvent.VK_Z){
-
+            if(pilaPowerUps.esVacia()!=true){
+                sizePowerUps-=1;
+                PowerUps powActual = pilaPowerUps.getTope().getDato();
+                pilaPowerUps.pop();
+                if(powActual.getTipo()==1){
+                    condicionEscudo =  true;
+                }
+                if(powActual.getTipo()==2){
+                    balasEspeciales = 2;
+                    Jugador.setDañoJugador(2);
+                }
+                if(powActual.getTipo()==3){
+                    balasEspeciales = 1;
+                    Jugador.setDañoJugador(3);
+                }
+            }
         }
         }
     //FIN DE INPUT DE USUARIO
